6524dcf72b8e603187ec1e281d59985f87dfb8b8
w-shimaya <integral2d@gmail.com>
2018-07-16 14:12:45 +0900

    44

--- 05/Chunk_41.py
+++ 05/Chunk_41.py
@@ -20,9 +20,28 @@ class Chunk:
         ret = ret + u'\n'
         return ret.encode('utf-8')
 
-def read_cabocha():
+    def surface(self):
+        ret = u''
+        for morph in self.morphs:
+            ret = ret + morph.surface
+        return ret
+
+    def has_verb(self):
+        for morph in self.morphs:
+            if morph.pos == u'動詞':
+                return True
+        return False
+
+    def has_noun(self):
+        for morph in self.morphs:
+            if morph.pos == u'名詞':
+                return True
+        return False
+            
+
+def read_cabocha(filename):
     ret = [] # list of sentences
-    fin = codecs.open('neko.txt.cabocha', 'r', 'utf-8')
+    fin = codecs.open(filename, 'r', 'utf-8')
     cont = fin.read().split('\n')
     fin.close()
 
@@ -44,10 +63,48 @@ def read_cabocha():
                 dst[chunk_mark.group(2)] = [int(chunk_mark.group(1))]
         morph_mark = re.match(ur'(.+)\t(.+),(.+),.+,.+,.+,.+,(.+),.+,.+', line)
         if morph_mark:
-            chunk.morphs.append(Morph(morph_mark.group(1), morph_mark.group(4), morph_mark.group(2), morph_mark.group(3)))
+            # exclude 記号
+            if morph_mark.group(2) != u'記号':
+                chunk.morphs.append(Morph(morph_mark.group(1), morph_mark.group(4), morph_mark.group(2), morph_mark.group(3)))
         if line == 'EOS':
-            sentence.append(chunk)
-            ret.append(sentence)
+            if chunk:
+                if chunk.morphs:
+                    sentence.append(chunk)
+            if sentence:
+                ret.append(sentence)
+            chunk = None
+            sentence = []
+            dst = {}
+    return ret
+
+def parse_cabocha(cont):
+    sentence = [] # list of Chunk
+    chunk = None
+    dst = {}
+    for line in cont:
+        chunk_mark = re.match(ur'\* ([0-9]+) (-?[0-9]+)D .*', line)
+        if chunk_mark:
+            if chunk:
+                sentence.append(chunk)
+            if chunk_mark.group(1) in dst:
+                chunk = Chunk([], int(chunk_mark.group(2)), dst[chunk_mark.group(1)])
+            else:
+                chunk = Chunk([], int(chunk_mark.group(2)))
+            if chunk_mark.group(2) in dst:
+                dst[chunk_mark.group(2)].append(int(chunk_mark.group(1)))
+            else:
+                dst[chunk_mark.group(2)] = [int(chunk_mark.group(1))]
+        morph_mark = re.match(ur'(.+)\t(.+),(.+),.+,.+,.+,.+,(.+),.+,.+', line)
+        if morph_mark:
+            # exclude 記号
+            if morph_mark.group(2) != u'記号':
+                chunk.morphs.append(Morph(morph_mark.group(1), morph_mark.group(4), morph_mark.group(2), morph_mark.group(3)))
+        if line == 'EOS':
+            if chunk:
+                if chunk.morphs:
+                    sentence.append(chunk)
+            if sentence:
+                ret.append(sentence)
             chunk = None
             sentence = []
             dst = {}
